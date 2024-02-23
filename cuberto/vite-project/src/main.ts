00063a1f156b0fcf531067b5a93ecc5e82dab0f7
Codewith-hamza <codeonlinesource@gmail.com>
2024-02-23 08:39:37 -0800

    completed home-page

--- cuberto/vite-project/src/main.ts
+++ cuberto/vite-project/src/main.ts
@@ -1,24 +1,21 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+Shery.mouseFollower();
+Shery.makeMagnet(".magnet-target" /* Element to target.*/, {
+  //Parameters are optional.
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
+});
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+Shery.hoverWithMediaCircle(".mask-target" /* Element to target.*/, {
+  // videos: ["/v2.mp4", "/v1.mp4", "/v2.mp4"],
+  images:["/2.jpg"]
+});
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+Shery.textAnimate(".text-target" /* Element to target.*/, {
+  //Parameters are optional.
+  style: 1,
+  y: 10,
+  delay: 0.1,
+  duration: 2,
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  multiplier: 0.1,
+});
